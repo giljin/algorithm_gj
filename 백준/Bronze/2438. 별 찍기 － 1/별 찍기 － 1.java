cb69e878cdf1d7356f02cef8124d27f41b25af36
giljin <68088970+giljin@users.noreply.github.com>
2023-03-04 21:19:44 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 300 ms, Memory: 18080 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        for(int i=0; i<count; i++){
+            for(int j=0; j<i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
