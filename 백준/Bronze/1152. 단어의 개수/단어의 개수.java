dc23c1252e8c0e8e1dc5d840d9bbb90366f8c594
giljin <68088970+giljin@users.noreply.github.com>
2023-03-28 13:36:43 +0900

    [Bronze II] Title: 단어의 개수, Time: 504 ms, Memory: 38356 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        if( input.isEmpty() ){
+            System.out.println(0);
+        }else{
+            System.out.println(input.split(" ").length);
+        }
+    }
+}
