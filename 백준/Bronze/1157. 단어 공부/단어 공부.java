3e2e3b78566d39524c2e438526079e79635a24b2
giljin <68088970+giljin@users.noreply.github.com>
2023-03-29 16:13:13 +0900

    [Bronze I] Title: 단어 공부, Time: 544 ms, Memory: 32720 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -3,31 +3,21 @@ public class Main {
 
     public static void main(String[] args)  {
         Scanner scanner =  new Scanner(System.in);
-        String lowerAlphabets = scanner.nextLine().toLowerCase();
-        String[] alphabetArray = lowerAlphabets.split("");
-        Map<String, Integer> map = new HashMap<>();
-        for (String alphabet : alphabetArray){
-            map.put(alphabet, map.get(alphabet) == null ? 1 : map.get(alphabet) + 1);
-        }
+        String upperCaseAlphabets = scanner.nextLine().toUpperCase();
+        int[] counts = new int[26];
+        int maxCount = 0, maxNo = 0;
+        for (int i=0; i<upperCaseAlphabets.length(); i++){
+            int x = upperCaseAlphabets.charAt(i);
+            counts[x-65]++;
 
-        int max = 0;
-        for(int count: map.values()){
-            if( count > max ){
-                max = count;
+            if( counts[x-65] > maxCount ){
+                maxNo = x;
+                maxCount = counts[x-65];
+            }else if( counts[x-65] == maxCount ){
+                maxNo = 63;
             }
         }
 
-        String result = "";
-        for (String alpha : map.keySet()){
-            if( map.get(alpha).intValue() == max ){
-                if( !result.isEmpty() ){
-                    result = "?";
-                    break;
-                }else{
-                    result = alpha;
-                }
-            }
-        }
-        System.out.println(result.toUpperCase());
+        System.out.println((char) maxNo);
     }
 }
