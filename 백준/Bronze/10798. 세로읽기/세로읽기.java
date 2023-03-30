8bc4f5dd8ecf9196c6e994a0375d1087601ee3b5
giljin <68088970+giljin@users.noreply.github.com>
2023-03-30 18:00:33 +0900

    [Bronze I] Title: 세로읽기, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -3,27 +3,18 @@ public class Main {
 
     public static void main(String[] args)  {
         Scanner scanner =  new Scanner(System.in);
-        List<String> words = new ArrayList<>();
-        int maxLength = -1;
+        String[] words = new String[5];
         for (int i = 0; i < 5; i++) {
-            String word = scanner.nextLine();
-            if( word.length() > maxLength ){
-                maxLength = word.length();
-            }
-            words.add(word);
+            words[i] = scanner.nextLine();
         }
 
-        String result = "";
-        for (int i = 0; i < maxLength; i++) {
-            for (int j = 0; j < words.size(); j++) {
-                String word = words.get(j);
-                if( word.length() <= i ){
-                    continue;
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if( words[j].length() > i ){
+                    System.out.print(words[j].charAt(i));
                 }
-                result += word.charAt(i);
             }
         }
-        System.out.println(result);
     }
 
 }
