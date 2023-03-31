1c5388cdbe35fc3a54e10f5c7a1696ec48738ef0
giljin <68088970+giljin@users.noreply.github.com>
2023-03-31 12:04:46 +0900

    [Bronze II] Title: 진법 변환, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,20 +1,8 @@
 import java.util.*;
 public class Main {
-
     public static void main(String[] args)  {
         Scanner scanner =  new Scanner(System.in);
-        int result =0;
-        String number = scanner.next();
-        int digit = scanner.nextInt();
-        for (int i = 0; i < number.length(); i++) {
-            int no = number.charAt(number.length()-1-i) - 48;
-            if( no > 9 ){
-                no -= 7;
-            }
-            result += no * Math.pow(digit, i);
-        }
-
-        System.out.println(result);
+        System.out.println(Integer.parseInt(scanner.next(), scanner.nextInt()));
     }
 
 }
