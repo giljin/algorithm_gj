0d07e6217943e599936b365b90c8b2f6e51522e1
giljin <68088970+giljin@users.noreply.github.com>
2023-04-04 16:37:43 +0900

    [Bronze I] Title: 소인수분해, Time: 252 ms, Memory: 18088 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -4,23 +4,13 @@ public class Main {
     public static void main(String[] args)  {
         Scanner scanner =  new Scanner(System.in);
         int n = scanner.nextInt();
-        List<Integer> divisors = new ArrayList<>();
+
+        int t = n;
         for (int i = 2; i <= n; i++) {
-            if( n % i == 0 ){
-                divisors.add(i);
+            while ( t % i == 0 ){
+                t /= i;
+                System.out.println(i);
             }
         }
-        while ( n > 1 ){
-            for (int i = 0; i < divisors.size(); i++) {
-                int divisor = divisors.get(i);
-                if( n % divisor == 0 ){
-                    System.out.println(divisor);
-                    n /= divisor;
-                    break;
-                }
-            }
-        }
-
     }
-
 }
