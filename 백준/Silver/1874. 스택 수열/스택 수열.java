30d963b07a881aa79986d9792be78c227586933a
giljin <68088970+giljin@users.noreply.github.com>
2023-04-19 17:03:40 +0900

    [Silver II] Title: 스택 수열, Time: 340 ms, Memory: 27392 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+        int start = 0;
+
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(br.readLine());
+
+            if(value > start) {
+                for(int j = start + 1; j <= value; j++) {
+                    stack.push(j);
+                    sb.append('+').append('\n');
+                }
+                start = value;
+            }else if(stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+        System.out.println(sb);
+    }
+}
