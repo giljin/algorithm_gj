79288dd22162cd9d4fe5a7234840473df2b9f8e7
giljin <68088970+giljin@users.noreply.github.com>
2023-05-17 15:37:11 +0900

    [Silver IV] Title: 동전 0, Time: 252 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
+        int result = 0;
+        while (K > 0){
+            for (int i = N-1; i >= 0 ; i--) {
+                if( coins[i] <= K ){
+                    K -= coins[i];
+                    result++;
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+
+    }
+
+
+}
