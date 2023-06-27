f738dcf754fd8629eafd916f2f9cf01d3dd28910
giljin <68088970+giljin@users.noreply.github.com>
2023-06-27 18:58:25 +0900

    [Silver II] Title: 주식, Time: 1108 ms, Memory: 350760 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
             String[] prices = br.readLine().split(" ");
@@ -22,7 +22,8 @@ public class Main {
                     result += max - currentPrice;
                 }
             }
-            System.out.println(result);
+            sb.append(result).append("\n");
         }
+        System.out.println(sb);
     }
 }
