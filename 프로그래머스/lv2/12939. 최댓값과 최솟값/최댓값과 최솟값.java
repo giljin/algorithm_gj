95e5319601ce3088c532d7bbbdffb797eb197c8b
giljin <68088970+giljin@users.noreply.github.com>
2023-07-05 19:22:46 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.08 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String s) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        String[] arr = s.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            int number = Integer.parseInt(arr[i]);
+            min = Math.min(min, number);
+            max = Math.max(max, number);
+        }
+
+        return min + " " + max;
+    }
+}
