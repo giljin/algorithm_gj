4938ba510cc1f428e2149cf8c9e4d4e9ab4e3ba1
giljin <68088970+giljin@users.noreply.github.com>
2023-07-29 00:01:43 +0900

    [level 2] Title: 2 x n 타일링, Time: 3.63 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] dp = new int[600001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+}
