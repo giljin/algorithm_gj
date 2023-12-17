6afffabe721aaed99508a79251621e5cbf8f57fb
나길진 <68088970+giljin@users.noreply.github.com>
2023-12-17 15:51:12 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 77.61 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        Queue<Long> q1 = arrayToQueue(queue1);
+        Queue<Long> q2 = arrayToQueue(queue2);
+
+        long sum1 = getQueueSum(q1);
+        long sum2 = getQueueSum(q2);
+
+        int answer = 0;
+        while (sum1 != sum2) {
+            if (answer > (q1.size() + q2.size() * 2)) {
+                return -1;
+            }
+
+            if (sum1 < sum2) {
+                long element = q2.poll();
+                q1.add(element);
+                sum1 += element;
+                sum2 -= element;
+            } else {
+                long element = q1.poll();
+                q2.add(element);
+                sum2 += element;
+                sum1 -= element;
+            }
+
+            answer++;
+        }
+
+        return answer;  
+    }
+    
+    public Queue<Long> arrayToQueue(int[] array) {
+        Queue<Long> queue = new LinkedList<>();
+        for (long number : array) {
+            queue.add(number);
+        }
+        return queue;
+    }
+
+    public long getQueueSum(Queue<Long> queue) {
+        return queue.stream()
+                .reduce(Long::sum)
+                .orElse(0L);
+    }
+}
