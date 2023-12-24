259b3295e7cc31620406c91c2f08b70a54af9175
나길진 <68088970+giljin@users.noreply.github.com>
2023-12-24 13:52:30 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653?language=java) 
 
 ### 성능 요약
 
-메모리: 76.5 MB, 시간: 0.02 ms
+메모리: 73.5 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 13:45:27
+2023년 12월 0일 13:52:26
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -2,30 +2,26 @@ class Solution {
     public int solution(int storey) {
         int answer = 0;
         
-        while( storey >= 10 ){
+        while( storey != 0 ){
             int modStorey = storey % 10;
-            storey /= 10;
+            
             if( modStorey == 5 ){
-                int nextStorey = storey % 10;
+                int nextStorey = storey / 10 % 10;
                 if( nextStorey < 5 ){
                     answer += modStorey;
                 }else{
-                    storey++;
+                    storey += 10;
                     answer += 10 - modStorey;
                 }
             }else if( modStorey < 5 ){
                 answer += modStorey;
             }else{
                 answer += 10 - modStorey;
-                storey++;
+                storey += 10;
             }
+            storey /= 10;
         }
-        
-        if( storey < 6 ){
-            answer += storey;
-        }else{
-            answer += 10 - storey + 1;
-        }
+
         return answer;
     }
 }
